f2b77e382100fc58d5f2658b005be49532fff128
PapanaDeepika <deepikapapana13@gmail.com>
2025-03-24 03:34:14 +0530

    FirstCommit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,31 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { View, Text, Button } from "react-native";
+import React from "react";
+import { Stack, router } from "expo-router";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
+export default function _layout() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "red",
+        },
+        headerTintColor: "white",
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          headerTitle: "Home",
+          headerRight: () => <Button onPress={() => {router.push('./contact')}} title="Contact" />,
+        }}
+      />
+      <Stack.Screen name="about" options={{ headerTitle: "About" }} />
+      <Stack.Screen
+        name="blog/index"
+        options={{ headerTitle: "All Blog Posts" }}
+      />
+      <Stack.Screen name="contact" options={{ headerTitle: "Contact", presentation: 'modal' }} />
+      <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
+    </Stack>
   );
 }
